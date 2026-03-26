6f02cc124ba872ac01d4407de4033f67a3d3919b
Artem Duda <user@gmail.com>
2026-03-26 14:38:12 +0200

    Expanded DAG with more DVC steps

--- dags/ml_training_pipeline.py
+++ dags/ml_training_pipeline.py
@@ -20,12 +20,13 @@ default_args = {
 
 
 def check_model_quality():
-    metrics_path = os.path.join(HOST_PROJECT_PATH, "reports", "metrics.json")
+    metrics_path = "/opt/airflow/reports/metrics.json"
 
     if not os.path.exists(metrics_path):
+        print(f"File not found: {metrics_path}")
         return "model_rejected"
 
-    with open(metrics_path, "r") as f:
+    with open(metrics_path, "r", encoding="utf-8") as f:
         metrics = json.load(f)
 
     accuracy = metrics.get("accuracy", 0)
@@ -40,48 +41,52 @@ def check_model_quality():
 with DAG(
     "ml_training_pipeline",
     default_args=default_args,
-    description="MLOps Pipeline: Check, Prepare, Train",
-    schedule_interval=timedelta(days=1),
+    description="MLOps Pipeline: Full DVC Flow",
+    schedule=timedelta(days=1),
     catchup=False,
 ) as dag:
 
     venv_mount = Mount(source="", target="/app/.venv", type="volume")
-
     project_mount = Mount(source=HOST_PROJECT_PATH, target="/app", type="bind")
 
-    check_data = DockerOperator(
+    docker_kwargs = {
+        "image": "mlops-lab-model:latest",
+        "docker_url": "unix://var/run/docker.sock",
+        "network_mode": "mlops-network",
+        "auto_remove": "force",
+        "mounts": [project_mount, venv_mount],
+    }
+
+    check_dvc = DockerOperator(
         task_id="check_dvc_status",
-        image="mlops-lab-model:latest",
-        # command='dvc status',
         command="rm -f .dvc/tmp/rwlock && dvc status",
-        docker_url="unix://var/run/docker.sock",
-        network_mode="bridge",
-        auto_remove="force",
-        mounts=[project_mount, venv_mount],
+        **docker_kwargs,
+    )
+
+    download_data = DockerOperator(
+        task_id="download_data", command="dvc repro download", **docker_kwargs
     )
 
     prepare_data = DockerOperator(
-        task_id="prepare_data",
-        image="mlops-lab-model:latest",
-        command="dvc repro prepare",
-        docker_url="unix://var/run/docker.sock",
-        network_mode="bridge",
-        auto_remove="force",
-        mounts=[project_mount, venv_mount],
+        task_id="prepare_data", command="dvc repro prepare", **docker_kwargs
+    )
+
+    test_data = DockerOperator(
+        task_id="test_data", command="dvc repro test_data", **docker_kwargs
     )
 
     train_model = DockerOperator(
         task_id="train_model",
-        image="mlops-lab-model:latest",
         command="dvc repro optimize",
-        docker_url="unix://var/run/docker.sock",
-        network_mode="mlops-network",
-        auto_remove="force",
-        mounts=[project_mount, venv_mount],
         environment={
             "MLFLOW_TRACKING_URI": "http://mlflow-server:5000",
             "UV_LINK_MODE": "copy",
         },
+        **docker_kwargs,
+    )
+
+    test_model = DockerOperator(
+        task_id="test_model", command="dvc repro test_model", **docker_kwargs
     )
 
     branching = BranchPythonOperator(
@@ -91,15 +96,20 @@ with DAG(
 
     register_model = DockerOperator(
         task_id="register_model",
-        image="mlops-lab-model:latest",
         command="mlflow models register -m 'models:/IMDB_RF_Model/latest' -n IMDB_Production_Model",
-        docker_url="unix://var/run/docker.sock",
-        network_mode="mlops-network",
-        auto_remove="force",
         environment={"MLFLOW_TRACKING_URI": "http://mlflow-server:5000"},
+        **docker_kwargs,
     )
 
     model_rejected = EmptyOperator(task_id="model_rejected")
 
-    check_data >> prepare_data >> train_model >> branching
+    (
+        check_dvc
+        >> download_data
+        >> prepare_data
+        >> test_data
+        >> train_model
+        >> test_model
+        >> branching
+    )
     branching >> [register_model, model_rejected]
